10d525993eb962f6b69a17dfaa5f1db966bfef07
Real Name <redkiaforte0506@gmail.com>
2025-07-25 17:11:57 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Python kernel for VS Code notebooks named 'test'";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+          ipykernel
+          pandas
+          numpy
+          # optionally add numpy, pandas, matplotlib, etc.
+        ]);
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "test";
+          packages = [ pythonEnv ];
+          shellHook = ''
+            # Register the kernel under the name "test" each time you enter the shell
+            ${pythonEnv.interpreter} -m ipykernel install --user --name=test --display-name "Python (test)"
+          '';
+        };
+      });
+}
