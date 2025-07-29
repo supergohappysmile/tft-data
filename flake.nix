6511fc9a031e055d827d5b069fae96257ca8aba3
Real Name <redkiaforte0506@gmail.com>
2025-07-29 22:02:50 +0000

    test

--- flake.nix
+++ flake.nix
@@ -14,19 +14,30 @@
           ipykernel
           notebook
           pandas
-          gspread
-          gspread_dataframe
-          # gspread-pandas
-          oauth2client
+           (pkgs.python311Packages.buildPythonPackage rec {
+            pname = "gspread";
+            version = "6.1.2"; # check PyPI for latest
+            src = pkgs.fetchPypi {
+              inherit pname version;
+              sha256 = "sha256-sUdoi4x6GMmDXV+ZiZfsF8l8BHC6vKsX9lrCs6MkArc=";
+            };
+            doCheck = false;
+            format = "setuptools"; # <-- this is usually what you need
+          })
+          # oauth2client
+          numpy
           pip
           # optionally add numpy, pandas, matplotlib, etc.
         ]);
       in {
+        packages.default = pythonEnv; # needs this line to be "nix build"-able
         devShells.default = pkgs.mkShell {
           name = "test";
           packages = [ pythonEnv ];
+          # echo "${builtins.concatStringsSep " " (map (p: p.pname or p.name) pythonEnv)}"
           shellHook = ''
-          echo "${pythonEnv}  <-- select this in notebook"
+          echo "${pythonEnv}  <-- 2select this in notebook"
+            # jupyter kernelspec remove -f tft-data >/dev/null 2>&1 || true
             # Register the kernel under the name "test" each time you enter the shell
             ${pythonEnv.interpreter} -m ipykernel install --user --name=test --display-name "${pythonEnv}"
           '';
