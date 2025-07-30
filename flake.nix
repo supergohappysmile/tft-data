17e82ab7533e431ce15bbf4efba7dc92ad706c8c
Real Name <redkiaforte0506@gmail.com>
2025-07-30 05:23:19 +0000

    working jupyter server

--- flake.nix
+++ flake.nix
@@ -1,46 +1,41 @@
 {
-  description = "Python kernel for VS Code notebooks named \"select this one for tft data\"";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
-          ipykernel
-          notebook
-          pandas
-           (pkgs.python311Packages.buildPythonPackage rec {
-            pname = "gspread";
-            version = "6.1.2"; # check PyPI for latest
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+        gspread-df =    pkgs.python313Packages.buildPythonPackage rec {
+            pname = "gspread-dataframe";
+            version = "4.0.0"; # check PyPI for latest
             src = pkgs.fetchPypi {
               inherit pname version;
-              sha256 = "sha256-sUdoi4x6GMmDXV+ZiZfsF8l8BHC6vKsX9lrCs6MkArc=";
+              sha256 = "sha256-XKVJNHjsrkm4M2ZKBrrI4XtbTtC9939VHJSPMFRYYOY=";
             };
             doCheck = false;
             format = "setuptools"; # <-- this is usually what you need
-          })
-          # oauth2client
-          numpy
-          pip
-          # optionally add numpy, pandas, matplotlib, etc.
-        ]);
-      in {
-        packages.default = pythonEnv; # needs this line to be "nix build"-able
-        devShells.default = pkgs.mkShell {
-          name = "test";
-          packages = [ pythonEnv ];
-          # echo "${builtins.concatStringsSep " " (map (p: p.pname or p.name) pythonEnv)}"
-          shellHook = ''
-          echo "${pythonEnv}  <-- 2select this in notebook"
-            # jupyter kernelspec remove -f tft-data >/dev/null 2>&1 || true
-            # Register the kernel under the name "test" each time you enter the shell
-            ${pythonEnv.interpreter} -m ipykernel install --user --name=test --display-name "${pythonEnv}"
-          '';
+          };
+    in
+      rec {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (python313.withPackages(ps: with ps; [
+              jupyter
+              ipykernel
+              numpy
+              pandas
+              oauth2client
+              gspread
+              gspread-df 
+              
+            ]))
+          ];
+          shellHook = "
+          jupyter notebook
+          ";
         };
-      });
-}
+      }
+    );
+}   
